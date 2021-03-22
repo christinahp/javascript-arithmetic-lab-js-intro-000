95ed75b9969af7140e4724f194c50e135d53f4f9
Christina Petersen <christina.h.petersen4@gmail.com>
2021-03-22 19:01:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add (x, y) {
+  return x + y
+}
+
+function subtract (x, y) {
+  return x - y
+}
+
+function multiply (x, y) {
+  return x * y
+}
+
+function divide (x, y) {
+  return x / y
+}
+
+function increment(n) {
+  n++
+  return n
+}
+
+function decrement(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+}
